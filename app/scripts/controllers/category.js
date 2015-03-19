75976ac97a6c3762028393525514ad35794e742f
aarongray <aaron@ponderyourpath.com>
2015-03-19 17:28:09 -0500

    Change back to the categories index after saving.

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -1,7 +1,7 @@
 'use strict';
 
-App.controller('CategoryCtrl', ['$scope', 'Category',
-  function ($scope, Category) {
+App.controller('CategoryCtrl', ['$scope', '$state', 'Category',
+  function ($scope, $state, Category) {
     $scope.category = new Category();           // create a new category in our $scope for use in the view
     $scope.categories = Category.query();       // assign all the categories to our $scope for use in the view
 
@@ -9,6 +9,7 @@ App.controller('CategoryCtrl', ['$scope', 'Category',
       $scope.category.$save();                  // save the category to the database on the server side
       $scope.categories.push($scope.category);  // add the category to the array on the client side
       $scope.category = new Category();
+      $state.go('categories');
     }
 
     $scope.delete = function(category){
